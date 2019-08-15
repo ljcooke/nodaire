0e930abdc03933d06fcff381ebe48d80d757374e
Liam Cooke <noreply@liamcooke.com>
2019-08-15 21:08:56 +1000

    Refactor

--- lib/nodaire.rb
+++ lib/nodaire.rb
@@ -1,3 +1,6 @@
 # frozen_string_literal: true
 
+module Nodaire
+end
+
 require 'nodaire/tablatal'
--- lib/nodaire/tablatal.rb
+++ lib/nodaire/tablatal.rb
@@ -3,32 +3,35 @@
 # Implementation of Tablatal
 # Tablatal is (c) Devine Lu Linvega (MIT License)
 
-module Nodaire
-  class Tablatal
-    class ParserError < StandardError; end
+class Nodaire::Tablatal
+  class ParserError < StandardError; end
 
-    def self.parse(string)
-      lines = (string || '').strip.split("\n")
-      return if lines.empty?
-      keys = make_keys lines.shift.scan(/(\S+\s*)/).flatten
-      lines.map do |line|
-        next if line.match(/^\s*(;.*)?$/)
-        keys.map { |key, range| [key, line[range].strip] }.to_h
-      end.compact
-    end
+  def self.parse(string)
+    lines = (string || '').strip.split("\n")
+                          .reject { |line| line.match(/^\s*(;.*)?$/) }
+    return if lines.empty?
 
-    def self.make_keys(segs)
+    keys = make_keys(lines.shift.scan(/(\S+\s*)/).flatten)
+    lines.map { |line| make_line(line, keys) }.compact
+  end
+
+  class << self
+    def make_keys(segs)
       [].tap do |keys|
         start = 0
         segs.each_with_index do |seg, idx|
           key = seg.strip.downcase.to_sym
           len = seg.size if idx < segs.size - 1
           raise ParserError, 'Duplicate keys' if keys.any? { |k| k[0] == key }
+
           keys.push [key, start...(len && start + len)]
           start += len if len
         end
       end
     end
-    private_class_method :make_keys
+
+    def make_line(line, keys)
+      keys.map { |key, range| [key, line[range].strip] }.to_h
+    end
   end
 end
