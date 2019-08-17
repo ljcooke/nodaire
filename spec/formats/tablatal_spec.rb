a2f782073e5f68bb88da751b2e548ed9662296f7
Liam Cooke <noreply@liamcooke.com>
2019-08-17 16:26:16 +1000

    Refactor Tablatal spec

--- spec/formats/tablatal_spec.rb
+++ spec/formats/tablatal_spec.rb
@@ -3,8 +3,6 @@
 require 'nodaire'
 
 describe Nodaire::Tablatal do
-  let(:instance) { described_class.parse(input) }
-
   let(:input) do
     <<~TBTL
       NAME    AGE   COLOR
@@ -25,44 +23,48 @@ describe Nodaire::Tablatal do
   end
 
   describe '.parse' do
-    let(:result) { described_class.parse(input) }
+    let(:return_value) { described_class.parse(input) }
 
     it 'returns an instance of the class' do
-      expect(result).to be_a described_class
+      expect(return_value).to be_a described_class
     end
   end
 
-  describe '#rows' do
-    it 'returns the expected output' do
-      expect(instance.rows).to eq expected_output
-    end
-  end
+  describe 'instance methods' do
+    let(:instance) { described_class.parse(input) }
 
-  describe '#to_a' do
-    it 'returns the expected output' do
-      expect(instance.to_a).to eq expected_output
-    end
-  end
-
-  describe '#keys' do
-    it 'returns the keys in the original order' do
-      expect(instance.keys).to eq %i[name age color]
-    end
-  end
-
-  describe '#to_csv' do
-    let(:expected_output) do
-      <<~CSV
-        name,age,color
-        Erica,12,Opal
-        Alex,23,"Cyan, Turquoise"
-        Nike,34,赤い
-        Ruca,45,Grey
-      CSV
+    describe '#rows' do
+      it 'returns the expected output' do
+        expect(instance.rows).to eq expected_output
+      end
     end
 
-    it 'returns the expected output' do
-      expect(instance.to_csv).to eq expected_output
+    describe '#to_a' do
+      it 'returns the expected output' do
+        expect(instance.to_a).to eq expected_output
+      end
+    end
+
+    describe '#keys' do
+      it 'returns the keys in the original order' do
+        expect(instance.keys).to eq %i[name age color]
+      end
+    end
+
+    describe '#to_csv' do
+      let(:expected_output) do
+        <<~CSV
+          name,age,color
+          Erica,12,Opal
+          Alex,23,"Cyan, Turquoise"
+          Nike,34,赤い
+          Ruca,45,Grey
+        CSV
+      end
+
+      it 'returns the expected output' do
+        expect(instance.to_csv).to eq expected_output
+      end
     end
   end
 end
