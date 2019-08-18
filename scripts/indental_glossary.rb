8875076b8dffe95c82bdc918d59116096cc1fc84
Liam Cooke <noreply@liamcooke.com>
2019-08-18 16:05:24 +1000

    Refactor glossary test script

--- scripts/indental_glossary.rb
+++ scripts/indental_glossary.rb
@@ -1,17 +1,18 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'open-uri'
-
 lib = File.expand_path('../lib', __dir__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
-require 'nodaire'
+require 'nodaire/indental'
+require 'open-uri'
 
-FILENAME = 'test/indental_glossary.ndtl'
+TMP_DIR = 'tmp'
+FILENAME = "#{TMP_DIR}/indental_glossary.ndtl"
 URL = 'https://wiki.xxiivv.com/scripts/database/glossary.ndtl'
 
-def read_input!
+def fetch_glossary!
+  Dir.mkdir(TMP_DIR) unless Dir.exist?(TMP_DIR)
   if File.exist?(FILENAME)
     puts "Reading from #{FILENAME}"
     File.open(FILENAME).read
@@ -23,12 +24,21 @@ def read_input!
   end
 end
 
-indental = Nodaire::Indental.parse read_input!
+def parse_glossary
+  input = fetch_glossary!
+  indental = Nodaire::Indental.parse(input)
 
-if indental.valid?
+  unless indental.valid?
+    indental.errors.each { |message| warn message }
+    return false
+  end
+
   indental.categories.each do |key|
     puts "#{key} (#{indental.data[key].size})"
   end
-else
-  indental.errors.each { |message| warn message }
+
+  true
 end
+
+success = parse_glossary
+exit(success ? 0 : 1)
