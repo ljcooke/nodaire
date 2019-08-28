ac601c56c0bb433294aa1f7dc6f821ff1b55d9ac
Liam Cooke <noreply@liamcooke.com>
2019-08-28 20:50:30 +1000

    Deprecate ParserError
    [ci skip]

--- lib/nodaire/errors.rb
+++ lib/nodaire/errors.rb
@@ -6,6 +6,7 @@ module Nodaire
   ##
   # This exception is raised if a parser error occurs.
   #
+  # @deprecated This will be removed in a future release.
   # @since 0.1.0
   #
   class ParserError < StandardError; end
